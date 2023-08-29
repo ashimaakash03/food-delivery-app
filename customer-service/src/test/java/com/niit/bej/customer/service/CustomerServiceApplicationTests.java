6b21faea47981f0717856de032ba0201230d1017
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-29 23:26:18 +0530

    created customer-service module

---
+++ customer-service/src/test/java/com/niit/bej/customer/service/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.customer.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

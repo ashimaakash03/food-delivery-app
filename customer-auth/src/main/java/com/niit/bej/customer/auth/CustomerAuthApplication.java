ce72c78f9e2ba770d5ca0fd61fb04db201794609
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-29 21:43:06 +0530

    created customer-auth module

---
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/CustomerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.customer.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerAuthApplication.class, args);
+    }
+
+}
---
+++ customer-auth/src/test/java/com/niit/bej/customer/auth/CustomerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niit.bej.customer.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

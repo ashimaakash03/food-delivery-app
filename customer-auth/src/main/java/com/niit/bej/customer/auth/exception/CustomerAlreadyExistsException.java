8d2e01abd17d0b7bd9e6e809e5bcc5186593159a
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 10:58:34 +0530

    renamed UserAlreadyExistsException.java class to CustomerAlreadyExistsException.java

--- customer-auth/src/main/java/com/niit/bej/customer/auth/exception/CustomerAlreadyExistsException.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/exception/CustomerAlreadyExistsException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(reason = "User Already Exists", code = HttpStatus.CONFLICT)
-public class UserAlreadyExistsException extends Exception {
-    public UserAlreadyExistsException(String message) {
+public class CustomerAlreadyExistsException extends Exception {
+    public CustomerAlreadyExistsException(String message) {
         super(message);
     }
 }

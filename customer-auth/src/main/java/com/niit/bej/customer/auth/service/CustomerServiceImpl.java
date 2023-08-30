04f4905333615a72fca7bfb29efdc28ea79807e9
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:06:37 +0530

    created CustomerServiceImpl.java class

---
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerServiceImpl.java
@@ -0,0 +1,15 @@
+package com.niit.bej.customer.auth.service;
+
+import com.niit.bej.customer.auth.model.Customer;
+
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public Customer registerCustomer(Customer customer) {
+        return null;
+    }
+
+    @Override
+    public boolean loginCustomer(Customer customer) {
+        return false;
+    }
+}

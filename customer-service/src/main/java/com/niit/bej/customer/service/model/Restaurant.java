50aa8878367a041ee297eaf7897054ee5f192145
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 23:09:37 +0530

    added private fields in Restaurant.java class

--- customer-service/src/main/java/com/niit/bej/customer/service/model/Restaurant.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/Restaurant.java
@@ -1,4 +1,11 @@
 package com.niit.bej.customer.service.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class Restaurant {
+    @MongoId
+    private long id;
+    private String name;
+    private String locationCity;
+
 }

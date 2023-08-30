8bd5dd349b639592e9bf50d85fb6a236c6b4d7e0
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 00:03:08 +0530

    generated getters and setters

--- customer-service/src/main/java/com/niit/bej/customer/service/model/MenuItem.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/MenuItem.java
@@ -18,4 +18,36 @@ public class MenuItem {
         this.category = category;
         this.price = price;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }

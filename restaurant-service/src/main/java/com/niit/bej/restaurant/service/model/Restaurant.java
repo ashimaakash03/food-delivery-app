0637568a8e6fe36229689be564a0e080430c4019
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:58:53 +0530

    added private fields in Restaurant.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
@@ -1,4 +1,8 @@
 package com.niit.bej.restaurant.service.model;
 
 public class Restaurant {
+    private long id;
+    private String restaurantName;
+    private String locationCity;
+    private double ratings;
 }

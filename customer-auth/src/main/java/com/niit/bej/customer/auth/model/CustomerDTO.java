bd8206e42fc70f010ba9514d0398cb61d018c718
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 10:46:39 +0530

    added no-args and all-args constructors

--- customer-auth/src/main/java/com/niit/bej/customer/auth/model/CustomerDTO.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/model/CustomerDTO.java
@@ -4,4 +4,13 @@ public class CustomerDTO {
     private String fullname;
     private String contact;
     private String email;
+
+    public CustomerDTO() {
+    }
+
+    public CustomerDTO(String fullname, String contact, String email) {
+        this.fullname = fullname;
+        this.contact = contact;
+        this.email = email;
+    }
 }

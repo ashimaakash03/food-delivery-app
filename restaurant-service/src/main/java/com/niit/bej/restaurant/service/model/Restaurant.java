85349cb9e6c02eeba0557ecd785862eb2a649514
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 15:03:37 +0530

    deleted Restaurant.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
+++
@@ -1,8 +0,0 @@
-package com.niit.bej.restaurant.service.model;
-
-public class Restaurant {
-    private long id;
-    private String restaurantName;
-    private String locationCity;
-    private double ratings;
-}

842bb99de1b6ea5ca3d562e0ac645fddbcc33bf7
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:36:37 +0530

    implemented generateToken() method

--- customer-auth/src/main/java/com/niit/bej/customer/auth/security/JwtSecurityTokenGenerator.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/security/JwtSecurityTokenGenerator.java
@@ -1,12 +1,20 @@
 package com.niit.bej.customer.auth.security;
 
 import com.niit.bej.customer.auth.model.Customer;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
+import java.util.Date;
 import java.util.Map;
 
 public class JwtSecurityTokenGenerator implements SecurityTokenGenerator {
     @Override
     public Map<String, String> generateToken(Customer customer) {
-        return null;
+        String jwtToken = Jwts.builder().setIssuedAt(new Date())
+                .setSubject(customer.getFullname()).signWith(SignatureAlgorithm.HS256, "password")
+                .compact();
+
+        return Map.of("token", jwtToken,
+                "message", customer.getFullname() + " logged in");
     }
 }

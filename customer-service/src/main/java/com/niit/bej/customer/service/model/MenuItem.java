4234324627150b4b549070d5249e8011e1ee60f1
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 00:01:34 +0530

    added private fields in MenuItem.java class

--- customer-service/src/main/java/com/niit/bej/customer/service/model/MenuItem.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/MenuItem.java
@@ -1,4 +1,11 @@
 package com.niit.bej.customer.service.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class MenuItem {
+    @MongoId
+    private long id;
+    private String name;
+    private String category;
+    private float price;
 }

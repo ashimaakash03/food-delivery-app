499dde0f4274f7f4314c318d2a873b041bfcfe8d
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:41:36 +0530

    added constructor dependencies to CustomerService and SecurityTokenGenerator added @Autowired annotation

--- customer-auth/src/main/java/com/niit/bej/customer/auth/controller/CustomerController.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/controller/CustomerController.java
@@ -1,4 +1,20 @@
 package com.niit.bej.customer.auth.controller;
 
+import com.niit.bej.customer.auth.security.SecurityTokenGenerator;
+import com.niit.bej.customer.auth.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
 public class CustomerController {
+    private final CustomerService customerService;
+    private final SecurityTokenGenerator securityTokenGenerator;
+
+    @Autowired
+    public CustomerController(CustomerService customerService, SecurityTokenGenerator securityTokenGenerator) {
+        this.customerService = customerService;
+        this.securityTokenGenerator = securityTokenGenerator;
+    }
 }

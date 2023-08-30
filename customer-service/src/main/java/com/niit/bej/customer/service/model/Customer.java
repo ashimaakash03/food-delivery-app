456cc5b0c47bf4530e4e15f872d0730eb45ede95
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 22:46:13 +0530

    generated no-args and all-args constructors

--- customer-service/src/main/java/com/niit/bej/customer/service/model/Customer.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/Customer.java
@@ -13,4 +13,17 @@ public class Customer {
     private String email;
     private List<Address> addresses;
     private List<Restaurant> restaurants;
+
+    public Customer() {
+    }
+
+    public Customer(String fullname, String contact, String email, List<Address> addresses, List<Restaurant> restaurants) {
+        this.fullname = fullname;
+        this.contact = contact;
+        this.email = email;
+        this.addresses = addresses;
+        this.restaurants = restaurants;
+    }
+
+
 }

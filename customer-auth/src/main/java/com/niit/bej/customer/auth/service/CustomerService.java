56e313ede04862ff236ed528477e78cd902fb5f4
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:17:49 +0530

    completed registerCustomer() and loginCustomer() method implementations in CustomerServiceImpl.java

--- customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerService.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerService.java
@@ -1,9 +1,12 @@
 package com.niit.bej.customer.auth.service;
 
+import com.niit.bej.customer.auth.exception.CustomerAlreadyExistsException;
+import com.niit.bej.customer.auth.exception.CustomerNotFoundException;
+import com.niit.bej.customer.auth.exception.InvalidCredentialsException;
 import com.niit.bej.customer.auth.model.Customer;
 
 public interface CustomerService {
-    Customer registerCustomer(Customer customer);
+    Customer registerCustomer(Customer customer) throws CustomerAlreadyExistsException;
 
-    boolean loginCustomer(Customer customer);
+    boolean loginCustomer(Customer customer) throws InvalidCredentialsException, CustomerNotFoundException;
 }
--- customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerServiceImpl.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerServiceImpl.java
@@ -1,9 +1,14 @@
 package com.niit.bej.customer.auth.service;
 
+import com.niit.bej.customer.auth.exception.CustomerAlreadyExistsException;
+import com.niit.bej.customer.auth.exception.CustomerNotFoundException;
+import com.niit.bej.customer.auth.exception.InvalidCredentialsException;
 import com.niit.bej.customer.auth.model.Customer;
 import com.niit.bej.customer.auth.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -15,12 +20,27 @@ public class CustomerServiceImpl implements CustomerService {
     }
 
     @Override
-    public Customer registerCustomer(Customer customer) {
-        return null;
+    public Customer registerCustomer(Customer customer) throws CustomerAlreadyExistsException {
+        Optional<Customer> optionalCustomer = this.customerRepository.findById(customer.getEmail());
+        if (optionalCustomer.isPresent()) {
+            throw new CustomerAlreadyExistsException("Customer with Email: " + customer.getEmail() + " already present");
+        } else {
+            return this.customerRepository.save(customer);
+        }
     }
 
     @Override
-    public boolean loginCustomer(Customer customer) {
-        return false;
+    public boolean loginCustomer(Customer customer) throws InvalidCredentialsException, CustomerNotFoundException {
+        Optional<Customer> optionalCustomer = this.customerRepository.findById(customer.getEmail());
+        if (optionalCustomer.isPresent()) {
+            Customer customerInDatabase = optionalCustomer.get();
+            if (customerInDatabase.getEmail().equals(customer.getEmail()) && customerInDatabase.getPassword().equals(customer.getPassword())) {
+                return true;
+            } else {
+                throw new InvalidCredentialsException("Incorrect Email and Password entered");
+            }
+        } else {
+            throw new CustomerNotFoundException("Customer with Email: " + customer.getEmail() + " not present in database");
+        }
     }
 }

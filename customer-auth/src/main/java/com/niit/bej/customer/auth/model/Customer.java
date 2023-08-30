da1fb5cd55f0e6fc4d00af5413bb4be2a799c4c5
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 10:31:48 +0530

    added private fields in Customer.java class

--- customer-auth/src/main/java/com/niit/bej/customer/auth/model/Customer.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/model/Customer.java
@@ -1,4 +1,8 @@
 package com.niit.bej.customer.auth.model;
 
 public class Customer {
+    private String fullname;
+    private String contact;
+    private String email;
+    private String password;
 }

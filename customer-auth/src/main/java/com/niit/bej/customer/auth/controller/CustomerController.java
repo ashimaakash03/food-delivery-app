8a327ed830ac2fe3c2c20b5401c3eb96a8b4c69d
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:51:32 +0530

    implemented registerCustomer() and loginCustomer() methods in CustomerController.java

--- customer-auth/src/main/java/com/niit/bej/customer/auth/controller/CustomerController.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/controller/CustomerController.java
@@ -1,10 +1,20 @@
 package com.niit.bej.customer.auth.controller;
 
+import com.niit.bej.customer.auth.exception.CustomerAlreadyExistsException;
+import com.niit.bej.customer.auth.exception.CustomerNotFoundException;
+import com.niit.bej.customer.auth.exception.InvalidCredentialsException;
+import com.niit.bej.customer.auth.model.Customer;
 import com.niit.bej.customer.auth.security.SecurityTokenGenerator;
 import com.niit.bej.customer.auth.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -16,5 +26,32 @@ public class CustomerController {
     public CustomerController(CustomerService customerService, SecurityTokenGenerator securityTokenGenerator) {
         this.customerService = customerService;
         this.securityTokenGenerator = securityTokenGenerator;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerCustomer(@RequestBody Customer customer) {
+        try {
+            Customer registeredCustomer = this.customerService.registerCustomer(customer);
+            return new ResponseEntity<>(registeredCustomer, HttpStatus.CREATED);
+        } catch (CustomerAlreadyExistsException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> loginCustomer(Customer customer) {
+        try {
+            boolean isCustomerLoggedIn = this.customerService.loginCustomer(customer);
+            if (isCustomerLoggedIn) {
+                Map<String, String> generatedToken = this.securityTokenGenerator.generateToken(customer);
+                return new ResponseEntity<>(generatedToken, HttpStatus.OK);
+            } else {
+                throw new InvalidCredentialsException("Incorrect Email And Password Combination");
+            }
+        } catch (InvalidCredentialsException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+        } catch (CustomerNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }

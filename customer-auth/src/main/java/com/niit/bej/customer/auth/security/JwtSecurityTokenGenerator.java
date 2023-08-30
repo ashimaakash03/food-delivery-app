1ea2ab214e44cbb51e1fdaf53761ccfefec8d466
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:37:09 +0530

    added @Service annotation

--- customer-auth/src/main/java/com/niit/bej/customer/auth/security/JwtSecurityTokenGenerator.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/security/JwtSecurityTokenGenerator.java
@@ -3,10 +3,12 @@ package com.niit.bej.customer.auth.security;
 import com.niit.bej.customer.auth.model.Customer;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.Map;
 
+@Service
 public class JwtSecurityTokenGenerator implements SecurityTokenGenerator {
     @Override
     public Map<String, String> generateToken(Customer customer) {

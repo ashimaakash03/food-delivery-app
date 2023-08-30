388d7a7e1d95d5a82a5fcab1e87654b8bf6198ea
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:05:48 +0530

    added abstract registerCustomer() and loginCustomer() methods

--- customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerService.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerService.java
@@ -1,4 +1,9 @@
 package com.niit.bej.customer.auth.service;
 
+import com.niit.bej.customer.auth.model.Customer;
+
 public interface CustomerService {
+    Customer registerCustomer(Customer customer);
+
+    boolean loginCustomer(Customer customer);
 }

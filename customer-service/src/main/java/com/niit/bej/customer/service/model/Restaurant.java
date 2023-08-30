a01e9bd69e7ac34bfae378c89962676c0672238a
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 23:11:56 +0530

    added orders as private field in Restaurant.java class

--- customer-service/src/main/java/com/niit/bej/customer/service/model/Restaurant.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/Restaurant.java
@@ -2,10 +2,12 @@ package com.niit.bej.customer.service.model;
 
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.util.List;
+
 public class Restaurant {
     @MongoId
     private long id;
     private String name;
     private String locationCity;
-
+    private List<MenuItem> orders;
 }

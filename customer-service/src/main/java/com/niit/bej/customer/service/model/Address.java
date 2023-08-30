1c817ea646c3f743efcf54ae8b0a88e70751a464
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 22:56:23 +0530

    added private fields in Address.java class

--- customer-service/src/main/java/com/niit/bej/customer/service/model/Address.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/Address.java
@@ -1,4 +1,14 @@
 package com.niit.bej.customer.service.model;
 
+
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class Address {
+    @MongoId
+    private String flatDetails;
+    private String soceityName;
+    private int sector;
+    private String cityName;
+    private String stateName;
+    private long pincode;
 }

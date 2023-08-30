cdee4d5b9458f19d6633e39afec6849a75f814c2
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:10:23 +0530

    added CustomerRepository as a dependency through constructor autowiring added @Autowired annotation

--- customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerServiceImpl.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/service/CustomerServiceImpl.java
@@ -1,10 +1,18 @@
 package com.niit.bej.customer.auth.service;
 
 import com.niit.bej.customer.auth.model.Customer;
+import com.niit.bej.customer.auth.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
     public Customer registerCustomer(Customer customer) {

9d25c41da5fe1e90be20c624eadf2f34b4457888
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 11:32:03 +0530

    added abstract generateToken() method in SecurityTokenGenerator.java interface

--- customer-auth/src/main/java/com/niit/bej/customer/auth/security/SecurityTokenGenerator.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/security/SecurityTokenGenerator.java
@@ -1,4 +1,9 @@
 package com.niit.bej.customer.auth.security;
 
+import com.niit.bej.customer.auth.model.Customer;
+
+import java.util.Map;
+
 public interface SecurityTokenGenerator {
+    Map<String, String> generateToken(Customer customer);
 }

7bd714a4ac8b6c33a09c68ec7da3806c472d5fe6
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 10:40:32 +0530

    added getters and setters

--- customer-auth/src/main/java/com/niit/bej/customer/auth/model/Customer.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/model/Customer.java
@@ -27,4 +27,36 @@ public class Customer {
         this.email = email;
         this.password = password;
     }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }

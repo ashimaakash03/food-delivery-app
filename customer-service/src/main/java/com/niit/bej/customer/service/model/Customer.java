30fb743698863ca4534629c8701e2aee0167aa62
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 21:37:59 +0530

    added private fields in Customer.java class

--- customer-service/src/main/java/com/niit/bej/customer/service/model/Customer.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/model/Customer.java
@@ -1,4 +1,12 @@
 package com.niit.bej.customer.service.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document
 public class Customer {
+    private String fullname;
+    private String contact;
+    @MongoId
+    private String email;
 }

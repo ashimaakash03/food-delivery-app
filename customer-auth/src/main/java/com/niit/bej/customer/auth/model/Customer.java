882857f1294765fd98a8efd63370e501ed027404
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-30 10:36:38 +0530

    added JPA annotations

--- customer-auth/src/main/java/com/niit/bej/customer/auth/model/Customer.java
+++ customer-auth/src/main/java/com/niit/bej/customer/auth/model/Customer.java
@@ -1,8 +1,20 @@
 package com.niit.bej.customer.auth.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "zwigato_users")
 public class Customer {
+    @Column(name = "full_name", nullable = false)
     private String fullname;
+    @Column(name = "contact", length = 10, unique = true, nullable = false)
     private String contact;
+    @Id
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
+    @Column(name = "password", length = 25, unique = true, nullable = false)
     private String password;
 }

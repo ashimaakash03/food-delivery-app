7ce1658a99dd3cff52bad270bc5233d4f2d8faad
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 10:53:09 +0530

    added abstract methods in CustomerService.java interface

--- customer-service/src/main/java/com/niit/bej/customer/service/service/CustomerService.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/service/CustomerService.java
@@ -1,4 +1,28 @@
 package com.niit.bej.customer.service.service;
 
+import com.niit.bej.customer.service.model.Customer;
+import com.niit.bej.customer.service.model.Restaurant;
+
+import java.util.List;
+
 public interface CustomerService {
+    Customer registerCustomer(Customer customer);
+
+    Restaurant addRestaurantUnderUser(Restaurant restaurant, String email);
+
+    Restaurant removeRestaurantForUser(long id, String email);
+
+    Restaurant viewRestaurantUnderUser(long id, String email);
+
+    List<Restaurant> viewAllRestaurantsUnderUser(String email);
+
+    boolean deleteRestaurantUnderUser(long id, String email);
+
+    Customer updateCustomerDetails(Customer customer, String email);
+
+    Customer fetchACustomer(String email);
+
+    List<Customer> fetchAllCustomers();
+
+    boolean deleteUser(String email);
 }

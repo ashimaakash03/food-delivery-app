7663ef7eb43603b55f864e61768e1e655fee8b6b
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 11:14:46 +0530

    added @FeignClient annotation

--- customer-service/src/main/java/com/niit/bej/customer/service/proxy/CustomerProxy.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/proxy/CustomerProxy.java
@@ -1,4 +1,7 @@
 package com.niit.bej.customer.service.proxy;
 
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "customer-auth-service")
 public interface CustomerProxy {
 }

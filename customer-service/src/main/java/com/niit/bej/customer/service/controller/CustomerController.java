2756d2d1a4ef2e9114df6cd13ddc88a738a40ed2
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 23:36:08 +0530

    completed all endpoints for Restaurant in CustomerController.java

--- customer-service/src/main/java/com/niit/bej/customer/service/controller/CustomerController.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/controller/CustomerController.java
@@ -1,9 +1,8 @@
 package com.niit.bej.customer.service.controller;
 
-import com.niit.bej.customer.service.exception.CustomerAlreadyPresentException;
-import com.niit.bej.customer.service.exception.CustomerNotFoundException;
-import com.niit.bej.customer.service.exception.EmptyDatabaseException;
+import com.niit.bej.customer.service.exception.*;
 import com.niit.bej.customer.service.model.Customer;
+import com.niit.bej.customer.service.model.Restaurant;
 import com.niit.bej.customer.service.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -68,6 +67,66 @@ public class CustomerController {
             return new ResponseEntity<>("Customer with Email: " + email + " deleted", HttpStatus.OK);
         } catch (CustomerNotFoundException exception) {
             return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/add/{email}")
+    public ResponseEntity<?> addRestaurantForCustomer(@RequestBody Restaurant restaurant, @PathVariable String email) {
+        try {
+            Restaurant addedRestaurant = this.customerService.addRestaurantUnderCustomer(restaurant, email);
+            return new ResponseEntity<>(addedRestaurant, HttpStatus.CREATED);
+        } catch (CustomerNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (RestaurantAlreadyPresentException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
+        }
+    }
+
+    @GetMapping("/view/{email}/{id}")
+    public ResponseEntity<?> fetchRestaurantUnderCustomerById(@PathVariable long id, @PathVariable String email) {
+        try {
+            Restaurant requestedRestaurant = this.customerService.viewRestaurantUnderCustomer(id, email);
+            return new ResponseEntity<>(requestedRestaurant, HttpStatus.FOUND);
+        } catch (RestaurantNotFoundException | CustomerNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (EmptyDatabaseException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @GetMapping("/view/all/{email}")
+    public ResponseEntity<?> fetchAllRestaurantsUnderCustomer(@PathVariable String email) {
+        try {
+            List<Restaurant> restaurantList = this.customerService.viewAllRestaurantsUnderCustomer(email);
+            return new ResponseEntity<>(restaurantList, HttpStatus.FOUND);
+        } catch (EmptyDatabaseException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/update/{email}")
+    public ResponseEntity<?> updateRestaurantForCustomerById(@RequestBody Restaurant restaurant, @PathVariable String email) {
+        try {
+            Restaurant updatedRestaurant = this.customerService.updateSingleRestaurantForCustomer(restaurant, email);
+            return new ResponseEntity<>(updatedRestaurant, HttpStatus.OK);
+        } catch (EmptyDatabaseException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
+        } catch (CustomerNotFoundException | RestaurantNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{email}/{id}")
+    public ResponseEntity<?> deleteRestaurantForCustomerById(@PathVariable String email, @PathVariable long id) {
+        try {
+            this.customerService.removeRestaurantForCustomer(id, email);
+            return new ResponseEntity<>("Restaurant with ID: " + id + " deleted for customer with Email: " + email, HttpStatus.OK);
+        } catch (RestaurantNotFoundException | CustomerNotFoundException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (EmptyDatabaseException exception) {
+            return new ResponseEntity<>(exception.getMessage(), HttpStatus.NO_CONTENT);
         }
     }
 }

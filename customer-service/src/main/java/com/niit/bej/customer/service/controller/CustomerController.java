3ad9b136973d182e26f21642e5ddce151ee7a4db
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 22:47:37 +0530

    added constructor dependency to CustomerService added @Autowired annotation

--- customer-service/src/main/java/com/niit/bej/customer/service/controller/CustomerController.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/controller/CustomerController.java
@@ -1,9 +1,17 @@
 package com.niit.bej.customer.service.controller;
 
+import com.niit.bej.customer.service.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/customer")
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }

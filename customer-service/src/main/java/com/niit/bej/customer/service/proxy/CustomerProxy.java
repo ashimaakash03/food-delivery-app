967bc4685a373dbb0627ed99793a71591c91ee28
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-01 00:30:23 +0530

    added @FeignClient annotation added method to registerCustomer() via proxy

--- customer-service/src/main/java/com/niit/bej/customer/service/proxy/CustomerProxy.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/proxy/CustomerProxy.java
@@ -1,4 +1,13 @@
 package com.niit.bej.customer.service.proxy;
 
+import com.niit.bej.customer.service.model.Customer;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "customer-auth")
 public interface CustomerProxy {
+    @PostMapping("/api/v1/auth/register")
+    ResponseEntity<?> registerCustomer(@RequestBody Customer customer);
 }

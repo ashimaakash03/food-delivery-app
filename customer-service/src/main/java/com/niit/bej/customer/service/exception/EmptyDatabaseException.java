cd023c0eef40f7fd0006942ef5cbfc1427cd2b00
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 10:59:29 +0530

    added @ResponseStatus annotation

--- customer-service/src/main/java/com/niit/bej/customer/service/exception/EmptyDatabaseException.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/exception/EmptyDatabaseException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.customer.service.exception;
 
-public class EmptyDatabaseException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Nothing Found in Database", code = HttpStatus.NO_CONTENT)
+public class EmptyDatabaseException extends Exception {
+    public EmptyDatabaseException(String message) {
+        super(message);
+    }
 }

44a36d12126c5f837ec0e75f884e3c56b389b0a4
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 11:25:51 +0530

    added constructor autowiring added @Service and @Autowired annotation to CustomerServiceImpl.java

--- customer-service/src/main/java/com/niit/bej/customer/service/service/CustomerServiceImpl.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/service/CustomerServiceImpl.java
@@ -2,10 +2,28 @@ package com.niit.bej.customer.service.service;
 
 import com.niit.bej.customer.service.model.Customer;
 import com.niit.bej.customer.service.model.Restaurant;
+import com.niit.bej.customer.service.proxy.CustomerProxy;
+import com.niit.bej.customer.service.repository.CustomerRepository;
+import com.niit.bej.customer.service.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestaurantRepository restaurantRepository;
+    private final CustomerProxy customerProxy;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository, RestaurantRepository restaurantRepository, CustomerProxy customerProxy) {
+        this.customerRepository = customerRepository;
+        this.restaurantRepository = restaurantRepository;
+        this.customerProxy = customerProxy;
+    }
+
     @Override
     public Customer registerCustomer(Customer customer) {
         return null;

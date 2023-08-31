75bea835ad9296d01816fbbf7404b53879859610
713068_Ashim <ashimaakash1997@gmail.com>
2023-08-31 10:19:32 +0530

    added @ResponseStatus annotation

--- customer-service/src/main/java/com/niit/bej/customer/service/exception/CustomerAlreadyPresentException.java
+++ customer-service/src/main/java/com/niit/bej/customer/service/exception/CustomerAlreadyPresentException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.customer.service.exception;
 
-public class CustomerAlreadyPresentException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Customer is already present", code = HttpStatus.CONFLICT)
+public class CustomerAlreadyPresentException extends Exception {
+    public CustomerAlreadyPresentException(String message) {
+        super(message);
+    }
 }

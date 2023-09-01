2d80fad6f5dd64f2d36bc2b4d5f5c97587986d95
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:05:31 +0530

    added fields to RestaurantOwner.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/RestaurantOwner.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/RestaurantOwner.java
@@ -1,4 +1,12 @@
 package com.niit.bej.restaurant.service.model;
 
+import java.util.List;
+
 public class RestaurantOwner {
+    private String email;
+    private String password;
+    private String ownerName;
+    private String phoneNumber;
+    private String gstNumber;
+    private List<Address> address;
 }

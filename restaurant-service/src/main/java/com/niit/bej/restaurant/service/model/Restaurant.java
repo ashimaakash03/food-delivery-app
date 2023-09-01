411ff9faaeeb64b06729c23ceae6ad1c8ad01ba3
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:16:38 +0530

    added equals and hashCode to Restaurant.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
@@ -1,6 +1,8 @@
 package com.niit.bej.restaurant.service.model;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Restaurant {
     private int restaurantId;
@@ -68,6 +70,32 @@ public class Restaurant {
 
     public void setMenuList(List<Menu> menuList) {
         this.menuList = menuList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Restaurant that = (Restaurant) o;
+
+        if (restaurantId != that.restaurantId) return false;
+        if (!Objects.equals(email, that.email)) return false;
+        if (!Objects.equals(restaurantName, that.restaurantName)) return false;
+        if (!Objects.equals(city, that.city)) return false;
+        if (!Arrays.equals(url, that.url)) return false;
+        return Objects.equals(menuList, that.menuList);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = restaurantId;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (restaurantName != null ? restaurantName.hashCode() : 0);
+        result = 31 * result + (city != null ? city.hashCode() : 0);
+        result = 31 * result + Arrays.hashCode(url);
+        result = 31 * result + (menuList != null ? menuList.hashCode() : 0);
+        return result;
     }
 
 }

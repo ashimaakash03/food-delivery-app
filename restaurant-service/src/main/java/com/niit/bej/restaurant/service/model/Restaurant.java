ef669716d5500b35e5f4491817f1e5c48a0308f1
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:16:06 +0530

    added getters and setters to Restaurant.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
@@ -22,4 +22,52 @@ public class Restaurant {
         this.menuList = menuList;
     }
 
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public void setRestaurantName(String restaurantName) {
+        this.restaurantName = restaurantName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public byte[] getUrl() {
+        return url;
+    }
+
+    public void setUrl(byte[] url) {
+        this.url = url;
+    }
+
+    public List<Menu> getMenuList() {
+        return menuList;
+    }
+
+    public void setMenuList(List<Menu> menuList) {
+        this.menuList = menuList;
+    }
+
 }

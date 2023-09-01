b462688d04b51803991f6fc78be9513d0c8739ce
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:15:26 +0530

    added fields to Restaurant.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Restaurant.java
@@ -1,5 +1,12 @@
 package com.niit.bej.restaurant.service.model;
 
-public class Restaurant {
+import java.util.List;
 
+public class Restaurant {
+    private int restaurantId;
+    private String email;
+    private String restaurantName;
+    private String city;
+    private byte[] url;
+    private List<Menu> menuList;
 }

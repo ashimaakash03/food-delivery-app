bba63aa08172093e6dc0ee55ec4f613c4dd3ed47
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 11:59:25 +0530

    added getters and setter methods to Address.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Address.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Address.java
@@ -15,4 +15,36 @@ public class Address {
         this.postcode = postcode;
     }
 
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getRoadName() {
+        return roadName;
+    }
+
+    public void setRoadName(String roadName) {
+        this.roadName = roadName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Long getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(Long postcode) {
+        this.postcode = postcode;
+    }
+
 }

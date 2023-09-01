5e6d26d4776a094671becab233564459d496edee
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:10:10 +0530

    added constructors to Menu.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Menu.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Menu.java
@@ -3,4 +3,13 @@ package com.niit.bej.restaurant.service.model;
 public class Menu {
     private String itemName;
     private double price;
+
+    public Menu() {
+    }
+
+    public Menu(String itemName, double price) {
+        this.itemName = itemName;
+        this.price = price;
+    }
+
 }

980d91bd5051df356575f5b2772253012cbfd792
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-01 22:46:20 +0530

    added @ResponseStatus annotation

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/ItemNotFoundException.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/ItemNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.restaurant.service.exception;
 
-public class ItemNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Item Not Found in the menu", code = HttpStatus.NOT_FOUND)
+public class ItemNotFoundException extends Exception {
+    public ItemNotFoundException(String message) {
+        super(message);
+    }
 }

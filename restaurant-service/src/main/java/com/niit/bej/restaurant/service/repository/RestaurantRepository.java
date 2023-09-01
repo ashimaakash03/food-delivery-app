f6ab940962c2d3456b69b96eaf1b195dcbd4aa7b
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 14:22:08 +0530

    added findByRestaurantNameContaining() method to RestaurantRepository.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
@@ -3,6 +3,9 @@ package com.niit.bej.restaurant.service.repository;
 import com.niit.bej.restaurant.service.model.Restaurant;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface RestaurantRepository extends MongoRepository<Restaurant, Integer> {
+    List<Restaurant> findByRestaurantNameContaining(String restaurantName);
 
 }

a9941cce1f994baf40ef38ecccf36fabc43180fa
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 14:24:02 +0530

    added @Repository annotation to RestaurantRepository.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/repository/RestaurantRepository.java
@@ -2,9 +2,11 @@ package com.niit.bej.restaurant.service.repository;
 
 import com.niit.bej.restaurant.service.model.Restaurant;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface RestaurantRepository extends MongoRepository<Restaurant, Integer> {
     List<Restaurant> findByRestaurantNameContaining(String restaurantName);
 
@@ -13,6 +15,8 @@ public interface RestaurantRepository extends MongoRepository<Restaurant, Intege
     Restaurant findByRestaurantName(String restaurantName);
 
     Restaurant findByEmail(String email);
+
     Restaurant findByCity(String city);
+
     List<Restaurant> findByEmailContaining(String email);
 }

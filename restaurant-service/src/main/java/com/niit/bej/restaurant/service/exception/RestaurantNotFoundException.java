f6ca0948b9241770268740bfcc67368fa2d173f3
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-01 20:14:42 +0530

    modified RestaurantNotFoundException.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/RestaurantNotFoundException.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/RestaurantNotFoundException.java
@@ -3,7 +3,7 @@ package com.niit.bej.restaurant.service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Restaurant does not exists!")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Restaurant does not exist!")
 public class RestaurantNotFoundException extends Exception {
     public RestaurantNotFoundException(String message){
         super(message);

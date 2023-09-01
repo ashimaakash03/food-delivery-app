2900f32c852dfeac5ccce50d85c8ea9626bf0804
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 15:24:00 +0530

    added @ResponseStatus annotation to RestaurantNotFoundException.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/RestaurantNotFoundException.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/RestaurantNotFoundException.java
@@ -1,5 +1,11 @@
 package com.niit.bej.restaurant.service.exception;
 
-public class RestaurantNotFoundException extends Exception {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(code = HttpStatus.CONFLICT, reason = "Restaurant does not exists!")
+public class RestaurantNotFoundException extends Exception {
+    public RestaurantNotFoundException(String message){
+        super(message);
+    }
 }

c8042d5b4e1eb8af2a6e6655b8e80c26d5dcf8c6
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:06:34 +0530

    added getters and setter to RestaurantOwner.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/RestaurantOwner.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/RestaurantOwner.java
@@ -22,4 +22,51 @@ public class RestaurantOwner {
         this.address = address;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getGstNumber() {
+        return gstNumber;
+    }
+
+    public void setGstNumber(String gstNumber) {
+        this.gstNumber = gstNumber;
+    }
+
+    public List<Address> getAddress() {
+        return address;
+    }
+
+    public void setAddress(List<Address> address) {
+        this.address = address;
+    }
 }

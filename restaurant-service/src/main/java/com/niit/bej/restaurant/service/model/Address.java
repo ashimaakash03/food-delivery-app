f6db4ecc13f14e5bebbe52da5159ea1b5f7ba494
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 11:59:01 +0530

    added constructor to Address.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Address.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Address.java
@@ -4,4 +4,15 @@ public class Address {
     private int houseNumber;
     private String roadName, city;
     private Long postcode;
+
+    public Address() {
+    }
+
+    public Address(int houseNumber, String roadName, String city, Long postcode) {
+        this.houseNumber = houseNumber;
+        this.roadName = roadName;
+        this.city = city;
+        this.postcode = postcode;
+    }
+
 }

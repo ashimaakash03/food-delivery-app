2f7c6ad9bad9f0c42cdb14ec6d80ef0553fecb57
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 14:54:16 +0530

    created addRestaurant() method to RestaurantService.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
@@ -1,4 +1,8 @@
 package com.niit.bej.restaurant.service.service;
 
+import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistException;
+import com.niit.bej.restaurant.service.model.Restaurant;
+
 public interface RestaurantService {
+    public Restaurant addRestaurant(Restaurant restaurant, byte[] bytes) throws RestaurantAlreadyExistException;
 }

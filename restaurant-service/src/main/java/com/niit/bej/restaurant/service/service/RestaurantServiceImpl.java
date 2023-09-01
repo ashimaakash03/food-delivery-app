5f43cabe21606980a1860822b33563dec4a5e2b5
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 15:56:48 +0530

    added @Service annotation to RestaurantServiceImpl.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantServiceImpl.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantServiceImpl.java
@@ -3,9 +3,11 @@ package com.niit.bej.restaurant.service.service;
 import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistException;
 import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
 import com.niit.bej.restaurant.service.model.Restaurant;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class RestaurantServiceImpl implements RestaurantService {
     @Override
     public Restaurant addRestaurant(Restaurant restaurant, byte[] bytes) throws RestaurantAlreadyExistException {

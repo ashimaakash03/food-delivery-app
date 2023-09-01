005984c56fc3ec84f495cec849cd7c635b65db07
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:11:07 +0530

    added equals and hashCode to Menu.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Menu.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/Menu.java
@@ -1,5 +1,7 @@
 package com.niit.bej.restaurant.service.model;
 
+import java.util.Objects;
+
 public class Menu {
     private String itemName;
     private double price;
@@ -26,6 +28,27 @@ public class Menu {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Menu menu = (Menu) o;
+
+        if (Double.compare(price, menu.price) != 0) return false;
+        return Objects.equals(itemName, menu.itemName);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = itemName != null ? itemName.hashCode() : 0;
+        temp = Double.doubleToLongBits(price);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 
 }

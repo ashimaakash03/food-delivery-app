a37c060261f649ccfff7c6b4db018405ffc7da36
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 15:49:49 +0530

    added findByName method to RestaurantService.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantService.java
@@ -1,8 +1,11 @@
 package com.niit.bej.restaurant.service.service;
 
 import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistException;
+import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
 import com.niit.bej.restaurant.service.model.Restaurant;
 
 public interface RestaurantService {
     public Restaurant addRestaurant(Restaurant restaurant, byte[] bytes) throws RestaurantAlreadyExistException;
+    public Restaurant findByName(String restaurantName) throws RestaurantNotFoundException;
+
 }

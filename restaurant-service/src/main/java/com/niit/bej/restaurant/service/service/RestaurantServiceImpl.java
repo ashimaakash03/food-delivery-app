bfa3c71c42ec26a1f325494c8e3f374088a206f6
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 15:59:29 +0530

    injected field dependency of restaurantRepository to RestaurantServiceImpl.java

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantServiceImpl.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/service/RestaurantServiceImpl.java
@@ -3,12 +3,18 @@ package com.niit.bej.restaurant.service.service;
 import com.niit.bej.restaurant.service.exception.RestaurantAlreadyExistException;
 import com.niit.bej.restaurant.service.exception.RestaurantNotFoundException;
 import com.niit.bej.restaurant.service.model.Restaurant;
+import com.niit.bej.restaurant.service.repository.RestaurantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class RestaurantServiceImpl implements RestaurantService {
+
+    @Autowired
+    private RestaurantRepository restaurantRepository;
+
     @Override
     public Restaurant addRestaurant(Restaurant restaurant, byte[] bytes) throws RestaurantAlreadyExistException {
         return null;

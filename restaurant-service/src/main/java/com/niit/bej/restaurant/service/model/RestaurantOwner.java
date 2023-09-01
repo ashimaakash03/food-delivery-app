336f32721966d12531f8be7135fc1651337ffd60
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-01 12:06:16 +0530

    added constructors to RestaurantOwner.java class

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/RestaurantOwner.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/model/RestaurantOwner.java
@@ -9,4 +9,17 @@ public class RestaurantOwner {
     private String phoneNumber;
     private String gstNumber;
     private List<Address> address;
+
+    public RestaurantOwner() {
+    }
+
+    public RestaurantOwner(String email, String password, String ownerName, String phoneNumber, String gstNumber, List<Address> address) {
+        this.email = email;
+        this.password = password;
+        this.ownerName = ownerName;
+        this.phoneNumber = phoneNumber;
+        this.gstNumber = gstNumber;
+        this.address = address;
+    }
+
 }

31ac7ef5367fb20dc7f8e9997d95ce851e02cf82
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-01 22:33:23 +0530

    added @ResponseStatus annotation

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/ItemAlreadyPresentException.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/exception/ItemAlreadyPresentException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.restaurant.service.exception;
 
-public class ItemAlreadyPresentException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Item already present in the menu", code = HttpStatus.CONFLICT)
+public class ItemAlreadyPresentException extends Exception {
+    public ItemAlreadyPresentException(String message) {
+        super(message);
+    }
 }

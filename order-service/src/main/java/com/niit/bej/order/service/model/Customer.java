debdaf9739e870e9e39fed62c6d91f4cab8c673d
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:39:51 +0530

    generated getters and setters

--- order-service/src/main/java/com/niit/bej/order/service/model/Customer.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Customer.java
@@ -18,4 +18,36 @@ public class Customer {
         this.email = email;
         this.contact = contact;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
 }

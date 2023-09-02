9a2c13562cb007dbe08a4f94b8be95fa2fc553b4
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:57:30 +0530

    added private variables in Order.java class added @Document and @MongoId annotations

--- order-service/src/main/java/com/niit/bej/order/service/model/Order.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Order.java
@@ -1,4 +1,15 @@
 package com.niit.bej.order.service.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
+
+@Document(collection = "orders")
 public class Order {
+    @MongoId
+    private long id;
+    private Customer customer;
+    private Address deliveryAddress;
+    private List<Restaurant> restaurant;
 }

48dcbc6c58cedf27e6d13d93c18ace9bb9783519
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 10:12:23 +0530

    added constructor dependency to restaurant service interface added @Autowired annotation

--- restaurant-service/src/main/java/com/niit/bej/restaurant/service/controller/RestaurantController.java
+++ restaurant-service/src/main/java/com/niit/bej/restaurant/service/controller/RestaurantController.java
@@ -1,9 +1,17 @@
 package com.niit.bej.restaurant.service.controller;
 
+import com.niit.bej.restaurant.service.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/restaurant")
 public class RestaurantController {
+    private final RestaurantService restaurantService;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
 }

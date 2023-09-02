7d263e09a9032261cef8cb119659c8166d3b188b
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:37:37 +0530

    added private variables in Customer.java class

--- order-service/src/main/java/com/niit/bej/order/service/model/Customer.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Customer.java
@@ -1,4 +1,11 @@
 package com.niit.bej.order.service.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class Customer {
+    @MongoId
+    private long id;
+    private String fullName;
+    private String email;
+    private String contact;
 }

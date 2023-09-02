7e0394c12fd972e86fef7360d92338c3cf3c0b86
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:00:53 +0530

    generated getters and setters

--- order-service/src/main/java/com/niit/bej/order/service/model/Item.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Item.java
@@ -18,4 +18,36 @@ public class Item {
         this.category = category;
         this.price = price;
     }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }

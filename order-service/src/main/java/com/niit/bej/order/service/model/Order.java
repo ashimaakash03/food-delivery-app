1a618fa27fd6d6a5ac3f358911363e6845a4447e
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-03 00:33:33 +0530

    generated equals(), hashCode() and toString() methods

--- order-service/src/main/java/com/niit/bej/order/service/model/Order.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Order.java
@@ -4,6 +4,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.util.List;
+import java.util.Objects;
 
 @Document(collection = "orders")
 public class Order {
@@ -11,16 +12,16 @@ public class Order {
     private long id;
     private Customer customer;
     private Address deliveryAddress;
-    private List<Restaurant> restaurant;
+    private List<Restaurant> restaurants;
 
     public Order() {
     }
 
-    public Order(long id, Customer customer, Address deliveryAddress, List<Restaurant> restaurant) {
+    public Order(long id, Customer customer, Address deliveryAddress, List<Restaurant> restaurants) {
         this.id = id;
         this.customer = customer;
         this.deliveryAddress = deliveryAddress;
-        this.restaurant = restaurant;
+        this.restaurants = restaurants;
     }
 
     public long getId() {
@@ -47,11 +48,34 @@ public class Order {
         this.deliveryAddress = deliveryAddress;
     }
 
-    public List<Restaurant> getRestaurant() {
-        return restaurant;
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
     }
 
-    public void setRestaurant(List<Restaurant> restaurant) {
-        this.restaurant = restaurant;
+    public void setRestaurant(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id == order.id && Objects.equals(customer, order.customer) && Objects.equals(deliveryAddress, order.deliveryAddress) && Objects.equals(restaurants, order.restaurants);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customer, deliveryAddress, restaurants);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customer=" + customer +
+                ", deliveryAddress=" + deliveryAddress +
+                ", restaurants=" + restaurants +
+                '}';
     }
 }

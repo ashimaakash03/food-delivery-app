713b1a129b99aca5897734205af678b0224bad15
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:16:10 +0530

    added private variables in Restaurant.java class

--- order-service/src/main/java/com/niit/bej/order/service/model/Restaurant.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Restaurant.java
@@ -1,4 +1,13 @@
 package com.niit.bej.order.service.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.util.List;
+
 public class Restaurant {
+    @MongoId
+    private long id;
+    private String name;
+    private String city;
+    private List<Item> order;
 }

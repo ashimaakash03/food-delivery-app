8f32b36e52c365e8de988bc537b10b6373edc18e
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:31:20 +0530

    added private variables in Address.java class

--- order-service/src/main/java/com/niit/bej/order/service/model/Address.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Address.java
@@ -1,4 +1,13 @@
 package com.niit.bej.order.service.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class Address {
+    @MongoId
+    private String flatDetails;
+    private String soceityName;
+    private int sector;
+    private String cityName;
+    private String stateName;
+    private long pincode;
 }

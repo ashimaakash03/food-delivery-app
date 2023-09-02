69f088249e34e6c6258c8fa9557f9646b0c8b923
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-02 23:00:10 +0530

    generated no-args and all-args constructors

--- order-service/src/main/java/com/niit/bej/order/service/model/Item.java
+++ order-service/src/main/java/com/niit/bej/order/service/model/Item.java
@@ -1,8 +1,21 @@
 package com.niit.bej.order.service.model;
 
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
 public class Item {
+    @MongoId
     private long id;
     private String name;
     private String category;
     private double price;
+
+    public Item() {
+    }
+
+    public Item(long id, String name, String category, double price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
 }

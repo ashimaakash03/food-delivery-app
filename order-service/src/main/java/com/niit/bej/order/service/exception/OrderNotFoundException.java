9e550a2d5044b60cea4b7777244d3c1042f5f0da
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-03 11:19:52 +0530

    added @ResponseStatus annotation

--- order-service/src/main/java/com/niit/bej/order/service/exception/OrderNotFoundException.java
+++ order-service/src/main/java/com/niit/bej/order/service/exception/OrderNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.order.service.exception;
 
-public class OrderNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Order Not Found", code = HttpStatus.NOT_FOUND)
+public class OrderNotFoundException extends Exception {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
 }

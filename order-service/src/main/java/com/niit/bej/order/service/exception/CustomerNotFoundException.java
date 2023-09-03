2870f205a47eb195cb5c6446f19c720e0baa77eb
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-03 11:00:22 +0530

    added @ResponseStatus annotation

--- order-service/src/main/java/com/niit/bej/order/service/exception/CustomerNotFoundException.java
+++ order-service/src/main/java/com/niit/bej/order/service/exception/CustomerNotFoundException.java
@@ -1,4 +1,11 @@
 package com.niit.bej.order.service.exception;
 
-public class CustomerNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(reason = "Customer Not Found", code = HttpStatus.NOT_FOUND)
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
 }

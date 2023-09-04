8760f26c7fdeae89eec8ec661cf4346a0d966ee7
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 15:15:37 +0530

    added @Configuration, @EnableAutoConfiguration and @LoadBalancerClient

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/CustomerServiceRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/CustomerServiceRouteConfig.java
@@ -1,4 +1,16 @@
 package com.niit.bej.api.gateway.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@LoadBalancerClient(name = "customer-service")
 public class CustomerServiceRouteConfig {
+
 }

3e5c3a3e19ecabe30adf2eb579f4dab6a01398a3
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 15:28:09 +0530

    added locateOrderServiceRoutes() to OrderServiceRouteConfig.java

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/OrderServiceRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/OrderServiceRouteConfig.java
@@ -1,12 +1,24 @@
 package com.niit.bej.api.gateway.config;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableAutoConfiguration
 @LoadBalancerClient
 public class OrderServiceRouteConfig {
-
+    @Bean
+    @LoadBalanced
+    public RouteLocator locateOrderServiceRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(
+                        routePredicate -> routePredicate.path("/order/**")
+                                .uri("lb://order-service")
+                ).build();
+    }
 }

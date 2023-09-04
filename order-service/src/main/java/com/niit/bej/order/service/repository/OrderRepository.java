a178a78beafd1ad3c4f22635aaa52eecd38c3889
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 11:24:01 +0530

    added findByEmail() method in OrderRepository.java

--- order-service/src/main/java/com/niit/bej/order/service/repository/OrderRepository.java
+++ order-service/src/main/java/com/niit/bej/order/service/repository/OrderRepository.java
@@ -5,6 +5,9 @@ import com.niit.bej.order.service.model.Order;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends MongoRepository<Order, Long> {
+    Optional<Order> findByEmail(String email);
 }

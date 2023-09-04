c206b32a9bcb74a3a758878d23dd323de415058e
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 15:19:03 +0530

    added @Configuration, @EnableAutoConfiguration and @LoadBalancerClient annotations to RestaurantServiceRouteConfig.java

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/RestaurantServiceRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/RestaurantServiceRouteConfig.java
@@ -1,4 +1,12 @@
 package com.niit.bej.api.gateway.config;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@LoadBalancerClient
 public class RestaurantServiceRouteConfig {
+
 }

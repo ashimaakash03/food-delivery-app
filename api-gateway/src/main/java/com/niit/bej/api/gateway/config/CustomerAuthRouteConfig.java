892db90d2eb9d545567713a1dccb598efacb3dec
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-04 13:57:05 +0530

    added @Configuration and @LoadBalancerClient annotations to CustomerAuthRouteConfig.java

--- api-gateway/src/main/java/com/niit/bej/api/gateway/config/CustomerAuthRouteConfig.java
+++ api-gateway/src/main/java/com/niit/bej/api/gateway/config/CustomerAuthRouteConfig.java
@@ -1,4 +1,22 @@
 package com.niit.bej.api.gateway.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@LoadBalancerClient(name = "customer-auth-service")
 public class CustomerAuthRouteConfig {
+//    @Bean
+//    @LoadBalanced
+//    public RouteLocator locateCustomerAuthMicroserviceRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+//        return routeLocatorBuilder.routes()
+//                .route(
+//                        routePredicate -> routePredicate.path("/home/**")
+//                                .uri("lb://customer-auth-service")
+//                ).build();
+//    }
 }

0095758551b1938f03147d4907ead6c7e00b2f89
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-05 23:08:31 +0530

    added @Autowired annotation

--- order-service/src/main/java/com/niit/bej/order/service/controller/OrderController.java
+++ order-service/src/main/java/com/niit/bej/order/service/controller/OrderController.java
@@ -1,9 +1,17 @@
 package com.niit.bej.order.service.controller;
 
+import com.niit.bej.order.service.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/order")
 public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 }

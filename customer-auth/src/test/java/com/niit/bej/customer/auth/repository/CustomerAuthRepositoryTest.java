5212050d2dc660bdd1abee5ffaeb969ac548bf30
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-06 10:32:10 +0530

    added @ExtendWith and @DataJpaTest annotations

--- customer-auth/src/test/java/com/niit/bej/customer/auth/repository/CustomerAuthRepositoryTest.java
+++ customer-auth/src/test/java/com/niit/bej/customer/auth/repository/CustomerAuthRepositoryTest.java
@@ -1,4 +1,10 @@
 package com.niit.bej.customer.auth.repository;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class CustomerAuthRepositoryTest {
 }

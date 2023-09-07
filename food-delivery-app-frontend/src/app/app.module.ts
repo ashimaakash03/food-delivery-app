96dd696d77149542f3dbd72cabe7fa22d607879f
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-07 14:34:00 +0530

    generated login component

--- food-delivery-app-frontend/src/app/app.module.ts
+++ food-delivery-app-frontend/src/app/app.module.ts
@@ -3,16 +3,18 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
 import {SearchComponent} from './components/search/search.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {RegisterComponent} from './components/register/register.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LoginComponent} from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,

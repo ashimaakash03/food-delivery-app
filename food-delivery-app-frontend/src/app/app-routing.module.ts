a72d208214466c6f15441a595d2291479ed7a5ff
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-07 13:26:03 +0530

    generated homepage component

--- food-delivery-app-frontend/src/app/app-routing.module.ts
+++ food-delivery-app-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Routes} from "@angular/router";
+import {HomepageComponent} from "./components/homepage/homepage.component";
 
-
-
+const routes: Routes = [
+  {path: "", redirectTo: "\home", pathMatch: "full"},
+  {path: "home", component: HomepageComponent}
+]
 @NgModule({
   declarations: [],
   imports: [
--- food-delivery-app-frontend/src/app/app.module.ts
+++ food-delivery-app-frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import {SearchComponent} from './components/search/search.component';
 import {RegisterComponent} from './components/register/register.component';
 import {AppRoutingModule} from './app-routing.module';
 import {RouterOutlet} from "@angular/router";
+import {HomepageComponent} from './components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,

5842a5f1fca2f6af8b0c97d8fcb8f522525a6434
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-07 14:20:14 +0530

    added registration form with validators

--- food-delivery-app-frontend/src/app/app.module.ts
+++ food-delivery-app-frontend/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import {MatSnackBarModule} from "@angular/material/snack-bar";
     SearchComponent,
     RegisterComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSnackBarModule
-  ],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatSnackBarModule,
+		MatInputModule,
+		ReactiveFormsModule
+	],
   providers: [],
   bootstrap: [AppComponent]
 })

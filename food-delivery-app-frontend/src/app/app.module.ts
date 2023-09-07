dd1a3773356cdcd3266b8690037e676a97bb87c1
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-07 11:27:42 +0530

    generated search component

--- food-delivery-app-frontend/src/app/app.module.ts
+++ food-delivery-app-frontend/src/app/app.module.ts
@@ -3,11 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './components/header/header.component';
+import {SearchComponent} from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule

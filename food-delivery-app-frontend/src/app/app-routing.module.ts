b2707fd8268cf4156eeeef9c3ca956a221b4613e
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-07 15:22:50 +0530

    added route to load NotFoundComponent

--- food-delivery-app-frontend/src/app/app-routing.module.ts
+++ food-delivery-app-frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {HomepageComponent} from "./components/homepage/homepage.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {NotFoundComponent} from "./components/not-found/not-found.component";
 
 const routes: Routes = [
 	{path: "", redirectTo: "\home", pathMatch: "full"},
 	{path: "home", component: HomepageComponent},
-	{path: "register", component: RegisterComponent}
+	{path: "register", component: RegisterComponent},
+	{path: "**", component: NotFoundComponent}
 ]
 @NgModule({
   declarations: [],

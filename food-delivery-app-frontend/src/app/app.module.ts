e0a65a56ddd793444b57b98c9ebd54d4206cd5e1
Ashutosh <ashutosh.k.work@gmail.com>
2023-09-07 13:15:24 +0530

    added angular material

--- food-delivery-app-frontend/src/app/app.module.ts
+++ food-delivery-app-frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {HeaderComponent} from './components/header/header.component';
 import {SearchComponent} from './components/search/search.component';
 import { RegisterComponent } from './components/register/register.component';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

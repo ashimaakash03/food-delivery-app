247504ceb3e68c51e33114c220356e35c605c165
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-10 19:15:50 +0530

    modified access to header links

--- food-delivery-app-frontend/src/app/app-routing.module.ts
+++ food-delivery-app-frontend/src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import {HomepageComponent} from "./components/homepage/homepage.component";
 import {RegisterComponent} from "./components/register/register.component";
 import {NotFoundComponent} from "./components/not-found/not-found.component";
 import {LoginComponent} from "./components/login/login.component";
+import {CartPageComponent} from "./components/cart-page/cart-page.component";
+import {ItemDetailsComponent} from "./components/item-details/item-details.component";
 
 const routes: Routes = [
 	{path: "", redirectTo: "\home", pathMatch: "full"},
 	{path: "home", component: HomepageComponent},
 	{path: "register", component: RegisterComponent},
 	{path: "login", component: LoginComponent},
+	{path: "cart", component: CartPageComponent},
+	{path: "details", component: ItemDetailsComponent},
 	{path: "**", component: NotFoundComponent}
 ]
 @NgModule({

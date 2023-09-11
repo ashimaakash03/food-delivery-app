fddb9b8cee2fc387068068cdd108ea9ea0d6cf6b
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-11 21:52:51 +0530

    added fields and constructor in food-item.ts

--- food-delivery-app-frontend/src/app/models/food-item.ts
+++ food-delivery-app-frontend/src/app/models/food-item.ts
@@ -1,2 +1,19 @@
 export class FoodItem {
+    id: number;
+    name: string;
+    category: string;
+    imageUrl: string;
+    price: number;
+    ratings: number;
+    isFavorite: boolean;
+
+    constructor(id: number, name: string, category: string, imageUrl: string, price: number, ratings: number, isFavorite: boolean) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.ratings = ratings;
+        this.isFavorite = isFavorite;
+    }
 }

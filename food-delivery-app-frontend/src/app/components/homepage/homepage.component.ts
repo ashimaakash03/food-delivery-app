57d305d049e7f6703536acc65322dbd6fe0445f5
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-13 00:08:49 +0530

    completed homepage of Zwigato app

--- food-delivery-app-frontend/src/app/components/homepage/homepage.component.ts
+++ food-delivery-app-frontend/src/app/components/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FoodItem} from "../../models/food-item";
+import {FoodService} from "../../services/food.service";
 
 
 @Component({
@@ -7,10 +9,17 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-    constructor() {
+	foods: FoodItem[] = []
+
+	constructor(private foodService: FoodService) {
     }
 
     ngOnInit(): void {
-
+		// @ts-ignore
+		this.foods = this.foodService.getAll().subscribe(
+			(data: FoodItem[]) => {
+				this.foods = data;
+				alert("Menu List Fetched");
+			});
     }
 }
--- food-delivery-app-frontend/src/app/services/food.service.ts
+++ food-delivery-app-frontend/src/app/services/food.service.ts
@@ -1,10 +1,18 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {FoodItem} from "../models/food-item";
+import {Observable} from "rxjs";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class FoodService {
-	constructor() {
+	url: string = "http://localhost:3000/menu-list";
+
+	constructor(private http: HttpClient) {
 	}
 
+	getAll(): Observable<FoodItem[]> {
+		return this.http.get<FoodItem[]>(this.url);
+	}
 }

f3672aab48ca081fb14ecd77068628da452840fe
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-13 12:38:46 +0530

    modified homepage component

--- food-delivery-app-frontend/src/app/components/homepage/homepage.component.ts
+++ food-delivery-app-frontend/src/app/components/homepage/homepage.component.ts
@@ -1,6 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {FoodItem} from "../../models/food-item";
-import {FoodService} from "../../services/food.service";
 
 
 @Component({
@@ -9,17 +7,8 @@ import {FoodService} from "../../services/food.service";
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-	foods: FoodItem[] = []
-
-	constructor(private foodService: FoodService) {
-    }
-
     ngOnInit(): void {
-		// @ts-ignore
-		this.foods = this.foodService.getAll().subscribe(
-			(data: FoodItem[]) => {
-				this.foods = data;
-				alert("Menu List Fetched");
-			});
+		throw new Error('Method not implemented.');
     }
+
 }

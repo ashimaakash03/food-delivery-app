be7e38ecdf663f5eb8363489b7c71e75133fb1a0
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-13 14:34:14 +0530

    added fields and constructor to restaurant.ts class

--- food-delivery-app-frontend/src/app/models/restaurant.ts
+++ food-delivery-app-frontend/src/app/models/restaurant.ts
@@ -1,2 +1,15 @@
 export class Restaurant {
+	id: number;
+	name: string;
+	imageUrl: string;
+	ratings: number;
+	deliveryTime: string;
+
+	constructor(id: number, name: string, imageUrl: string, ratings: number, deliveryTime: string) {
+		this.id = id;
+		this.name = name;
+		this.imageUrl = imageUrl;
+		this.ratings = ratings;
+		this.deliveryTime = deliveryTime;
+	}
 }

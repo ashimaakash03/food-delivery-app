aaad74aaba0ab1c18297dd4c1051551b95aa0673
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-14 11:23:17 +0530

    completed restaurant.ts and restaurant.json files

--- food-delivery-app-frontend/src/app/models/restaurant.ts
+++ food-delivery-app-frontend/src/app/models/restaurant.ts
@@ -1,15 +1,19 @@
+import {FoodItem} from "./food-item";
+
 export class Restaurant {
 	id: number;
 	name: string;
 	imageUrl: string;
 	ratings: number;
+	menu: FoodItem[];
 	deliveryTime: string;
 
-	constructor(id: number, name: string, imageUrl: string, ratings: number, deliveryTime: string) {
+	constructor(id: number, name: string, imageUrl: string, ratings: number, menu: FoodItem[], deliveryTime: string) {
 		this.id = id;
 		this.name = name;
 		this.imageUrl = imageUrl;
 		this.ratings = ratings;
+		this.menu = menu;
 		this.deliveryTime = deliveryTime;
 	}
 }

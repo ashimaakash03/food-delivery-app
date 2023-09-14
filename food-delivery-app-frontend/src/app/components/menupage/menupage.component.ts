d54edbae9497a42f312a3d3f41123f42300fe9be
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-14 12:47:48 +0530

    modified menupage display

--- food-delivery-app-frontend/src/app/components/menupage/menupage.component.ts
+++ food-delivery-app-frontend/src/app/components/menupage/menupage.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FoodItem} from "../../models/food-item";
+import {FoodService} from "../../services/food.service";
 
 @Component({
 	selector: 'app-menupage',
@@ -6,7 +8,18 @@ import {Component, OnInit} from '@angular/core';
 	styleUrls: ['./menupage.component.css']
 })
 export class MenupageComponent implements OnInit {
+	menuitems: FoodItem[] = [];
+
+	constructor(private foodService: FoodService) {
+	}
 	ngOnInit(): void {
+		// @ts-ignore
+		this.menuitems = this.foodService.getAll().subscribe(
+			(data: FoodItem[]) => {
+				this.menuitems = data;
+				alert("Menu List Fetched");
+			}
+		);
 	}
 
 }

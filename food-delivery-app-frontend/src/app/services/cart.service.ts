668909b06d67ac07368d70c0bc2fa667288c71f2
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-24 23:07:22 +0530

    completed all methods in cart service

--- food-delivery-app-frontend/src/app/services/cart.service.ts
+++ food-delivery-app-frontend/src/app/services/cart.service.ts
@@ -1,10 +1,51 @@
 import {Injectable} from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+import {FoodItem} from "../models/food-item";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class CartService {
 
+	public cartItemList: FoodItem[] = [];
+	public foodList = new BehaviorSubject<any>([]);
+
 	constructor() {
+	}
+
+	getFoods() {
+		return this.foodList.asObservable();
+	}
+
+	setFood(food: any) {
+		this.cartItemList.push(food);
+		this.foodList.next(food);
+	}
+
+	addToCart(food: FoodItem) {
+		this.cartItemList.push(food);
+		this.foodList.next(this.cartItemList);
+		this.getTotalPrice();
+	}
+
+	getTotalPrice(): number {
+		let totalPrice: number = 0;
+		this.cartItemList.map((item: FoodItem) => {
+			totalPrice += item.price;
+		});
+		return totalPrice;
+	}
+
+	removeCartItem(food: FoodItem) {
+		this.cartItemList.map((item: FoodItem, index: number) => {
+			if (food.id === item.id) {
+				this.cartItemList.splice(index, 1);
+			}
+		});
+	}
+
+	clearCart() {
+		this.cartItemList = [];
+		this.foodList.next(this.cartItemList);
 	}
 }

a332057558767ca0f0cdf2392d407646d0f4c319
713068_Ashim <ashimaakash1997@gmail.com>
2023-09-27 12:27:46 +0530

    modified homepage display

--- food-delivery-app-frontend/src/app/models/restaurant.ts
+++ food-delivery-app-frontend/src/app/models/restaurant.ts
@@ -7,13 +7,15 @@ export class Restaurant {
 	ratings: number;
 	menu: FoodItem[];
 	deliveryTime: string;
+	city: string;
 
-	constructor(id: number, name: string, imageUrl: string, ratings: number, menu: FoodItem[], deliveryTime: string) {
+	constructor(id: number, name: string, imageUrl: string, ratings: number, menu: FoodItem[], deliveryTime: string, city: string) {
 		this.id = id;
 		this.name = name;
 		this.imageUrl = imageUrl;
 		this.ratings = ratings;
 		this.menu = menu;
 		this.deliveryTime = deliveryTime;
+		this.city = city;
 	}
 }
